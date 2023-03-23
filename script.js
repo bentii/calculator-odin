988c0b9b22acc0b064af8fad73b884d06111fecf
bentii <bentivegna666@gmail.com>
2023-03-23 00:17:31 -0300

    feat: CSS and Size Fixes

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let conta = [];
 let counter = 0;
-let counter2 = 0;
 let equacao;
 let ponto;
 sigDisabler();
+document.getElementById("openPara").disabled = false;
 document.getElementById("back").disabled = true;
+document.querySelectorAll("button").forEach(btn => btn.classList.add("button-28"));
 
 function inputDig(x) {
   const tela = document.getElementById("tela");
@@ -12,35 +13,36 @@ function inputDig(x) {
   if (document.getElementById("tela2").textContent != "") {
     inputReset();
   }
-  if(ponto == 1){
+  if (ponto == 1) {
     conta[--counter] = `${conta[counter]}.${x}`;
     document.getElementById(`dig${counter}`).textContent = conta[counter];
     counter++;
     ponto = 2;
-  } else if(ponto == 2){
+  } else if (ponto == 2) {
     conta[--counter] = `${conta[counter]}${x}`;
     document.getElementById(`dig${counter}`).textContent = conta[counter];
     counter++;
-  }else{
-  dig.classList.add("dig");
-  dig.id = `dig${counter}`;
-  dig.textContent = `${x}`;
-  tela.appendChild(dig);
-  conta[counter] = x;
-  counter++;
-  ponto = 0;
+  } else {
+    dig.classList.add("dig");
+    dig.id = `dig${counter}`;
+    dig.textContent = `${x}`;
+    tela.appendChild(dig);
+    conta[counter] = x;
+    counter++;
+    ponto = 0;
   }
 
-  document.getElementById("back").disabled = false;
   sigEnabler();
-  if(x == "0"){
+  document.getElementById("back").disabled = false;
+  document.getElementById("openPara").disabled = true;
+  if (x == "0") {
     document.getElementById("div").disabled = true;
   }
   console.log(conta);
   console.log(counter);
 }
 
-function inputPonto(){
+function inputPonto() {
   ponto = 1;
   sigDisabler();
   document.getElementById("back").disabled = true;
@@ -59,44 +61,60 @@ function inputSig(x) {
   ponto = 0;
   sigDisabler();
   document.getElementById("openPara").disabled = false;
-  if(x == "!"){
+  if (x == "!") {
+    digDisabler();
     document.getElementById("openPara").disabled = true;
     document.getElementById("add").disabled = false;
     document.getElementById("sub").disabled = false;
     document.getElementById("mult").disabled = false;
     document.getElementById("div").disabled = false;
+    document.getElementById("result").disabled = false;
   }
-  if(x == "/"){
-    document.getElementById("n0").disabled = true;
-  }
-  if(x == "("){
+  if (x == "(") {
     document.getElementById("closePara").disabled = false;
   }
-  if(x == ")"){
+  if (x == ")") {
+    sigEnabler();
+    digDisabler();
     document.getElementById("result").disabled = false;
+    document.getElementById("ponto").disabled = true;
+  }
+  if(x == "^"){
+    digEnabler();
+    sigDisabler();
+  }
+  if (x == "*" || x == "+" || x == "-" || x == "/"){
+    digEnabler();
   }
 }
 
 function inputBack() {
   --counter;
   conta.pop();
-  if(document.getElementById(`dig${counter - 1}`)){
-  if(document.getElementById(`dig${counter - 1}`).classList.contains("dig") == true){
-    sigEnabler();
-    digEnabler();
-  }else{
-    digEnabler();
-    sigDisabler();
+  if (document.getElementById(`dig${counter - 1}`)) {
+    if (
+      document.getElementById(`dig${counter - 1}`).classList.contains("dig") ==
+      true
+    ) {
+      sigEnabler();
+      digEnabler();
+    } else {
+      digEnabler();
+      sigDisabler();
+    }
   }
-}
-  if(document.getElementById(`dig${counter}`).classList.contains("digPonto") == true){
-    document.querySelectorAll(`.digPonto${counter}`).forEach(x => x.remove());
-  }else{
-  document.getElementById(`dig${counter}`).remove();
+  if (
+    document.getElementById(`dig${counter}`).classList.contains("digPonto") ==
+    true
+  ) {
+    document.querySelectorAll(`.digPonto${counter}`).forEach((x) => x.remove());
+  } else {
+    document.getElementById(`dig${counter}`).remove();
   }
   if (document.getElementById("tela").textContent == "") {
     document.getElementById("back").disabled = true;
     sigDisabler();
+    document.getElementById("openPara").disabled = false;
   }
   console.log(conta);
   console.log(counter);
@@ -107,6 +125,7 @@ function inputReset() {
   document.querySelectorAll("dig").forEach((item) => item.remove());
   document.querySelectorAll("sig").forEach((item) => item.remove());
   document.getElementById("tela2").textContent = "";
+  document.getElementById("tela").classList.remove("telaConta");
   conta = [];
   equacao = 0;
   console.log(conta);
@@ -114,6 +133,8 @@ function inputReset() {
   console.log(equacao);
   sigDisabler();
   document.getElementById("back").disabled = true;
+  digEnabler();
+  document.getElementById("openPara").disabled = false;
 }
 
 function inputResult() {
@@ -129,6 +150,7 @@ function inputResult() {
   calculos(equacao);
   console.log(equacao);
   document.getElementById("tela2").textContent = equacao;
+  document.getElementById("tela").classList.add("telaConta");
   sigDisabler();
   document.getElementById("back").disabled = true;
 }
@@ -226,19 +248,19 @@ function factorial(n) {
   }
 }
 
-function digEnabler(){
-document.getElementById("n0").disabled = false;
-document.getElementById("n1").disabled = false;
-document.getElementById("n2").disabled = false;
-document.getElementById("n3").disabled = false;
-document.getElementById("n4").disabled = false;
-document.getElementById("n5").disabled = false;
-document.getElementById("n6").disabled = false;
-document.getElementById("n7").disabled = false;
-document.getElementById("n8").disabled = false;
-document.getElementById("n9").disabled = false;
+function digEnabler() {
+  document.getElementById("n0").disabled = false;
+  document.getElementById("n1").disabled = false;
+  document.getElementById("n2").disabled = false;
+  document.getElementById("n3").disabled = false;
+  document.getElementById("n4").disabled = false;
+  document.getElementById("n5").disabled = false;
+  document.getElementById("n6").disabled = false;
+  document.getElementById("n7").disabled = false;
+  document.getElementById("n8").disabled = false;
+  document.getElementById("n9").disabled = false;
 }
-function digDisabler(){
+function digDisabler() {
   document.getElementById("n0").disabled = true;
   document.getElementById("n1").disabled = true;
   document.getElementById("n2").disabled = true;
@@ -251,7 +273,7 @@ function digDisabler(){
   document.getElementById("n9").disabled = true;
 }
 
-function sigEnabler(){
+function sigEnabler() {
   document.getElementById("ponto").disabled = false;
   document.getElementById("add").disabled = false;
   document.getElementById("sub").disabled = false;
@@ -263,7 +285,7 @@ function sigEnabler(){
   document.getElementById("closePara").disabled = false;
   document.getElementById("result").disabled = false;
 }
-function sigDisabler(){
+function sigDisabler() {
   document.getElementById("ponto").disabled = true;
   document.getElementById("add").disabled = true;
   document.getElementById("sub").disabled = true;
@@ -275,3 +297,84 @@ function sigDisabler(){
   document.getElementById("closePara").disabled = true;
   document.getElementById("result").disabled = true;
 }
+
+document.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  switch (event.keyCode) {
+    case 13:
+      document.getElementById("result").click();
+      break;
+    case 48:
+      if (event.shiftKey) {
+        document.getElementById("closePara").click();
+      } else {
+        document.getElementById("n0").click();
+      }
+      break;
+    case 49:
+      if (event.shiftKey) {
+        document.getElementById("fact").click();
+      } else {
+        document.getElementById("n1").click();
+      }
+      break;
+    case 50:
+      document.getElementById("n2").click();
+      break;
+    case 51:
+      document.getElementById("n3").click();
+      break;
+    case 52:
+      document.getElementById("n4").click();
+      break;
+    case 53:
+      document.getElementById("n5").click();
+      break;
+    case 54:
+      document.getElementById("n6").click();
+      break;
+    case 55:
+      document.getElementById("n7").click();
+      break;
+    case 56:
+      if (event.shiftKey) {
+        document.getElementById("mult").click();
+      } else {
+        document.getElementById("n8").click();
+      }
+      break;
+    case 57:
+      if (event.shiftKey) {
+        document.getElementById("openPara").click();
+      } else {
+        document.getElementById("n9").click();
+      }
+      break;
+    case 187:
+      if (event.shiftKey) {
+        document.getElementById("add").click();
+      }
+      break;
+    case 189:
+      document.getElementById("sub").click();
+      break;
+    case 191:
+      document.getElementById("div").click();
+      break;
+    case 222:
+      if (event.shiftKey) {
+        document.getElementById("pot").click();
+      }
+      break;
+    case 190:
+      document.getElementById("ponto").click();
+      break;
+    case 8:
+      if (event.shiftKey) {
+        document.getElementById("reset").click();
+      } else {
+        document.getElementById("back").click();
+      }
+      break;
+  }
+});
